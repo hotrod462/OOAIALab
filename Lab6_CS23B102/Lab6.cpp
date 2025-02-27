8c8d2fdeeb97347b31a766998840fae47fa90708
Aryan Jain <145840890+hotrod462@users.noreply.github.com>
2025-02-27 16:21:37 +0530

    fix quicksort imple

--- Lab6_CS23B102/Lab6.cpp
+++ Lab6_CS23B102/Lab6.cpp
@@ -2,36 +2,76 @@
 
 using namespace std;
 struct CurrencySorter{
-    void operator()(vector<int> &nums){
+    void operator()(vector<long long int> &nums){
         quicksort(nums, 0, nums.size() -1);
     }
-    void quicksort(vector<int> &tosort, int l, int r){
-        if( l == r) return;
-        int i =l;
-        int j =l;
-        
-        int pivot = tosort[r-1]; //last element pivot
-        while(j< r-1){
-            if(tosort[j] > pivot) j++;
-            else {
-                swap(tosort[i], tosort[j]);
+    long long int partition(vector<long long int> &arr, int l, int r){
+        long long int pivot = arr[r];
+
+        long long int i = l -1;
+        for(int j = l; j<=r -1; j++){
+            if(arr[j]<= pivot){
                 i++;
-                j++;
+                swap(arr[i], arr[j]);
             }
         }
-        swap(tosort[i], tosort[r-1]);// put pivot in right place
-        quicksort(tosort, l, i-1 );
-        quicksort(tosort, i+1, r);
+        swap(arr[i+1], arr[r]);
+        return i+1;
+
+    }
+    long long int randomPartition(vector<long long int> &arr , int l, int r ){
+        //reference - GFG
+        srand(time(NULL));
+        int random = l + rand() % (r-l);
+        swap(arr[random], arr[r]);
+        return partition(arr, l, r);
+    }
+    void quicksort(vector<long long int> &tosort, int l, int r){
+        if(l < r){
+            long long int partidx = randomPartition(tosort, l, r);
+
+            quicksort(tosort, l, partidx -1);
+            quicksort(tosort, partidx +1, r);
+        }
     }
 };
 
 struct FibGenerator{
+    vector<vector<int>> res;
+    void matMul(vector<vector<long long int>> &A, vector<vector<long long int>> &B, vector<vector<long long int>>& C){
+        int N = 2;
+        for(int ii = 0; ii < N; ii++ ){
+            for(int jj = 0; jj < N; jj++){
+                for(int kk =0 ; kk < N; kk++){
+                    C[ii][jj] = A[ii][kk] + B[kk][jj];
+                }
+            }
+        }
+    }
     int operator()(int i){
+
         return fib(i);
     }
 
-    int fib(int i){
-        
+    vector<vector<long long int>>matPower(vector<vector<long long int>> &a, long long int n){
+        if(n == 1) return a;
+        else{
+            vector<vector<long long int>> x = matPower(a, n/2);
+            vector<vector<long long int>> sqr;
+            matMul(x, x, sqr);
+            if( n % 2 ==0 ){
+                return sqr;
+            }
+        else {
+            matMul(sqr, a, sqr);
+            return sqr;
+        }
+        }
+    }
+    long long int fib( long long int i){
+        vector<vector<long long int>> fibrec {{1, 1}, {1, 0}};
+        auto final_mat = matPower(fibrec, i);
+        return final_mat[0][0] + final_mat[0][1];
     }
 };
 
@@ -43,25 +83,35 @@ class PrimeCalculator{
 
 
     }
-    void printPrimes();
+    void printPrimes(){
+        for(auto ele: primes){
+            cout<<ele<<" "; }
+        cout<<endl;
+    }
 
-    void printPrimeSum(); 
+    void printPrimeSum(){
+        long long int sum;
+        for(auto ele: primes){
+            sum += ele;
+        }
+    }
 };
 
 class NumberAnalyzer{
     public:
+    long long int x;
     bool hasSqrFactor;
     set<unsigned int> divisor;
 
-    NumberAnalyzer(){
+    NumberAnalyzer(long long int a):x(a){
         hasSqrFactor = false;
     }
-    void findDivisors(unsigned int n){
-        for(unsigned int i = 1; i <=(unsigned int)sqrt(n);i++){
-            if(n % i == 0){
-                if(n %(i*i) == 0) hasSqrFactor = true;
+    void findDivisors(){
+        for(unsigned int i = 1; i <=(unsigned int)sqrt(x);i++){
+            if(x % i == 0){
+                if(x %(i*i) == 0) hasSqrFactor = true;
                 divisor.insert(i);
-                divisor.insert(n/i);
+                divisor.insert(x/i);
             }
         }
     }
@@ -84,3 +134,63 @@ class NumberAnalyzer{
 
 };
 
+
+int main(){
+    while(true){
+        int pick;
+        cin>>pick;
+
+        int t;
+        cin>>t;
+        //for(int ii = 0; ii < t; ii++){
+                if(pick == 1){
+                    int n;
+                    cin>>n;
+                    vector<long long int> currency(n);
+                    for(int i = 0; i < n; i++){
+                        long long int ele;
+                        cin>>ele;
+                        currency[i] = ele;
+                    }
+                    CurrencySorter c;
+                    c(currency);
+                    for(auto ele: currency){
+                        cout<<ele<<" ";
+                    }
+                }
+
+                if (pick == 2)
+                {
+                    long long int i;
+                    cin >> i;
+                    FibGenerator f;
+                    cout << f(i) << endl;
+                }
+                if (pick == 3)
+                {
+                    PrimeCalculator p;
+                    p.printPrimes();
+                    p.printPrimeSum();
+                }
+                if(pick ==4){
+                    string command;
+                    cin>>command;
+                    long long int x;
+                    cin>>x;
+                    
+                    NumberAnalyzer numal(x);
+                    if(command == "isSqareFree"){
+                        numal.isSquareFree();
+                    }
+                    else if(command == "countDivisors"){
+                        numal.countDivisors();
+                    }
+                    else{
+                        numal.sumOfDivisors();
+                    }
+                }
+        
+                
+            //}
+        }
+    }
